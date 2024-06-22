4ce160c8af86d05c8a9280ab3dfc5f3380bb14fc
Brandon <brandon@friendlystudios.net>
2024-06-22 10:42:58 -0500

    Updated player controller. created Interaction Component and created PlayerJump signal + Various cleanup

---
+++ Core/ComponentNode.h
@@ -0,0 +1,19 @@
+//
+// Created by ion on 6/21/24.
+//
+
+#ifndef COMPONENTNODE_H
+#define COMPONENTNODE_H
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/node.hpp>
+
+using namespace godot;
+
+
+class ComponentNode : public Node {
+};
+
+
+
+#endif //COMPONENTNODE_H
