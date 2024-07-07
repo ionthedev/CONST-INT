19a3b7f044a1b1c65b173eb13dcc7326efab19d4
Brandon <brandon@friendlystudios.net>
2024-07-06 23:56:38 -0500

    Created Inventory Component. It dynamically creates a grid as well as reading what slot is being hovered over.
    Created boiler plate for ItemDatabase and InventoryData so the InventoryComponent class can actually be usable.

---
+++ Config/InventoryData.h
@@ -0,0 +1,24 @@
+//
+// Created by ion on 7/6/24.
+//
+
+#ifndef INVENTORYDATA_H
+#define INVENTORYDATA_H
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/classes/resource_loader.hpp>
+#include <godot_cpp/classes/scene_tree.hpp>
+#include <godot_cpp/classes/grid_container.hpp>
+#include <godot_cpp/classes/texture_rect.hpp>
+
+using namespace godot;
+
+class InventoryData : public Object {
+    //TODO: Create data structure to store item id and position
+
+};
+
+
+
+#endif //INVENTORYDATA_H
---
+++ Core/itemDatabase.h
@@ -0,0 +1,24 @@
+//
+// Created by ion on 7/6/24.
+//
+
+#ifndef ITEMDATABASE_H
+#define ITEMDATABASE_H
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/classes/control.hpp>
+#include <godot_cpp/classes/resource_loader.hpp>
+#include <godot_cpp/classes/scene_tree.hpp>
+#include <godot_cpp/classes/grid_container.hpp>
+#include <godot_cpp/classes/texture_rect.hpp>
+
+using namespace godot;
+
+class ItemDatabase : public Resource {
+    //TODO: Create Item data struct to store necessary data. This also needs to hold information for if the item is stackable or combines with another item to create something new.
+
+};
+
+
+
+#endif //ITEMDATABASE_H
