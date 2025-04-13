16c9a6c364e1199ca61b4fc7fdf3436878e5969e
Brandon <brandon@friendlystudios.net>
2025-04-13 13:57:04 -0500

    Actually fixed demo project

--- godot/DemoAssets/PlayerDefaults/DebugMenu.gd
+++ godot/DemoAssets/PlayerDefaults/DebugMenu.gd
@@ -6,6 +6,8 @@ extends Control
 @export var velocity: Label
 @export var _Player: Player
 
+# This script is just a simple debug script to show certain exposed variables
+# and how the Player node can be interacted with.
 func _process(delta: float) -> void:
 	InputDir.text = str(_Player.calc_input_dir())
 	WishDir.text = str(_Player.calc_wish_dir())
